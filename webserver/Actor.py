966b887238b35232e487e07496b97dca0d8233e5
Victoria Yang <victoria.j.yang@gmail.com>
2018-11-16 14:07:30 -0500

    update actor with correct values

--- webserver/Actor.py
+++ webserver/Actor.py
@@ -1,11 +1,15 @@
 class Actor:
     def __init__(self, row):
-        self.year = row[0]
-        self.name = row[1]
+        self.film_name = row[1]
         self.film_imdblink = row[3]
         self.filmmaker = row[5]
-        self.filmmaker_imdblink = row[6]
-        self.gmap_url = GMAP_PREFIX.format(row[7], row[8])
-        self.location = row[12]
-        self.borough = row[13]
-        self.neighborhood = row[14]
+        self.filmmaker_imdblink = row[4]
+        self.actor_imdblink = row[8]
+        self.actor_name = row[10]
+        self.actor_gender = row[11]
+        self.actor_img = row[12]
+        self.character_name = row[14]
+        self.company_imdblink = row[17]
+        self.company_name = row[18]
+
+        
