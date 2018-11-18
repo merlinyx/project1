9221a23f4304c803f79b19cfb1380bd610f02bab
Yuxuan Mei <yuxuan.mei@icloud.com>
2018-11-18 18:49:49 -0500

    last fix 2

--- webserver/Actor.py
+++ webserver/Actor.py
@@ -2,13 +2,9 @@ class Actor:
     def __init__(self, row):
         self.film_name = row[1]
         self.film_imdblink = row[3]
-        self.filmmaker = row[5]
-        self.filmmaker_imdblink = row[4]
-        self.actor_imdblink = row[8]
-        self.actor_name = row[10]
-        self.actor_gender = row[11]
-        self.actor_img = row[12]
-        self.character_name = row[14]
-
-
+        self.actor_imdblink = row[6]
+        self.actor_name = row[8]
+        self.actor_gender = row[8]
+        self.actor_img = row[10]
+        self.character_name = row[12]
         
