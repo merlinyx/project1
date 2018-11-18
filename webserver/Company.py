c162db85081bc5d4bb1daae0adbab1d85e8ccd09
Victoria <victoria.j.yang@gmail.com>
2018-11-18 18:56:37 +0000

    fixed company link

--- webserver/Company.py
+++ webserver/Company.py
@@ -2,5 +2,5 @@ class Company:
     def __init__(self, row):
         self.film_name = row[1]
         self.film_imdblink = row[3]
-        self.company_imdblink = row[7]
-        self.company_name = row[6]
+        self.company_imdblink = row[6]
+        self.company_name = row[7]
